fa99f1ee7f8c7872d756eafbb8a97c4174300054
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-03 12:43:19 +1100

    Add test for duplicate links

--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -5,8 +5,17 @@ RSpec.feature "Creating a new link" do
     visit "/"
     fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
     click_button "Shorten"
-    expect(page).to have_content("Your link has been shortened.")
-    link = Link.first
-    expect(page).to have_content("Your short link is #{link.long_url}")
+    expect(page).to have_content("Your short link is short.ly/1")
+  end
+
+  scenario "doesn't create duplicated entries" do
+    visit "/"
+    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
+    click_button "Shorten"
+    expect(page).to have_content("Your short link is short.ly/1")
+    visit "/"
+    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
+    click_button "Shorten"
+    expect(page).to_not have_content("Your short link is short.ly/2")
   end
 end
