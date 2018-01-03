fd4bdedd0e1e2b3d361272a383e32c92c64b0f59
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-03 12:44:49 +1100

    Make long_url unique

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102033300) do
+ActiveRecord::Schema.define(version: 20180103005454) do
 
   create_table "links", force: :cascade do |t|
     t.string "long_url"
-    t.string "short_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["long_url"], name: "index_links_on_long_url", unique: true
   end
 
 end
