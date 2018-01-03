e4a30cad0fac75f6019f6e90ca4d58748b525d7f
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-03 15:39:46 +1100

    Remove redundant test

--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -7,15 +7,4 @@ RSpec.feature "Creating a new link" do
     click_button "Shorten"
     expect(page).to have_content("Your short link is short.ly/1")
   end
-
-  scenario "doesn't create duplicated entries" do
-    visit "/"
-    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
-    click_button "Shorten"
-    expect(page).to have_content("Your short link is short.ly/1")
-    visit "/"
-    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
-    click_button "Shorten"
-    expect(page).to_not have_content("Your short link is short.ly/2")
-  end
 end
