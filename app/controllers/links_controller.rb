1516edc664484d0c7b965fad9c318a18d2018fb0
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-03 12:46:00 +1100

    Create strong params, add find_or_create_by method for long_url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,13 @@ class LinksController < ApplicationController
   end
 
   def create
-    link = Link.create(long_url: params[:long_url])
-    render plain: "Your link has been shortened. Your short link is #{link.long_url}"
+    link = Link.find_or_create_by(link_params)
+    render plain: "Your link has been shortened. Your short link is #{link.short_url}"
+  end
+
+  private
+
+  def link_params
+    params.permit(:long_url)
   end
 end
