c3b0e2e1df8098d62fcf2477eea93af083fb6f5e
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-05 13:32:09 +1100

    Remove index on long_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103005454) do
+ActiveRecord::Schema.define(version: 20180105022859) do
 
   create_table "links", force: :cascade do |t|
     t.string "long_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["long_url"], name: "index_links_on_long_url", unique: true
   end
 
 end
