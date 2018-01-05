1782bde1ac207b1712f2ed1101dbf3a0519a3dde
Namibia Torres <namibialt@gmail.com>
2018-01-05 11:21:35 +1100

    Completed 4th part of the HW

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,10 +3,15 @@ class LinksController < ApplicationController
   end
 
   def create
-    link = Link.new(long_url: params[:long_url])
-    link.create_short_url
-    # binding.pry
-    link.save
-    render plain: "Your link has been shortened. Your short link is #{link.short_url}"
+    existing_link = Link.where(long_url: params[:long_url]).first
+    if existing_link
+      render plain: "Your short link is #{existing_link.short_url}"
+    else
+      link = Link.new(long_url: params[:long_url])
+      link.create_short_url
+      # binding.pry
+      link.save
+      render plain: "Your link has been shortened. Your short link is #{link.short_url}"
+    end
   end
 end
--- spec/features/link_feature_spec.rb
+++ spec/features/link_feature_spec.rb
@@ -10,3 +10,17 @@ RSpec.feature "Creating a new link" do
     expect(page).to have_content("Your short link is #{link.short_url}")
   end
 end
+
+RSpec.feature "When entering an entering an existing link" do
+  scenario "returns the corresponding short url" do
+    visit "/"
+    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
+    click_button "Shorten"
+    visit "/"
+    fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
+    click_button "Shorten"
+    link = Link.where(long_url: "http://ryanbigg.com/2016/04/hiring-juniors")
+    expect(link.count).to eq(1)
+    expect(page).to have_content("Your short link is #{link.first.short_url}")
+  end
+end
