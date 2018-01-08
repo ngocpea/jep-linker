f55a1fa48d12eec8d7edd41985e2c5acd2627ffc
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 16:37:51 +1100

    Add test for links forward action

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
+  let(:long_url) { "google.com" }
   it "can create a link successfully" do
-    post :create, params: { link: { long_url: "google.com" } }
-    link = Link.find_by(long_url: "google.com")
+    post :create, params: { link: { long_url: long_url } }
+    link = Link.find_by(long_url: long_url)
     expect(link).to be_present
     expect(response).to redirect_to(link_path(link))
   end
 
   it "shows a link" do
-    post :create, params: { link: { long_url: "google.com" } }
-    link = Link.find_by(long_url: "google.com")
+    post :create, params: { link: { long_url: long_url } }
+    link = Link.find_by(long_url: long_url)
     get :show, params: { id: link.id }
     expect(response).to be_success
+  end
+
+  it "forwards a short url to long url" do
+    link = Link.create(long_url: long_url)
+    get :forward, params: { short_url: link.short_url }
+    expect(response).to redirect_to("http://#{long_url}")
   end
 end
