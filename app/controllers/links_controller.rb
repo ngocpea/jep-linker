6389df8e0d064333523c993808e53099d97405de
Julie Huang <julie@juliehuang.co.nz>
2018-01-08 13:20:18 +1100

    Add test for checking error message

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,22 @@
 class LinksController < ApplicationController
   def index
+    @links = Link.all
   end
 
   def create
-    link = Link.create(long_url: params[:long_url])
-    render plain: "Your link has been shortened. Your short link is #{link.long_url}"
+    link = Link.find_or_initialize_by(long_url: params[:long_url])
+    if link.persisted?
+      render plain: "Your link has been shortened. Your short link is #{link}"
+      return
+    end
+
+    if link.save
+      render plain: "Your link has been successfully saved"
+    else
+      render plain: "Failed to save - please enter your link"
+    end
+
+    # link = Link.create(long_url: params[:long_url])
+    # render plain: "Your link has been shortened. Your short link is #{link.long_url}"
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,9 @@
 class Link < ApplicationRecord
-  before_save { self.short_url = SecureRandom.urlsafe_base64(6) }
+  # do I still need to add validation helpers here if already using find_or_create_by method?
+  #attr_reader :short_url
+  validates :long_url, uniqueness: true, presence: true
+
+  def shorten_url
+    short_url = "http://short.ly/" + id.to_s(16)
+  end
 end
