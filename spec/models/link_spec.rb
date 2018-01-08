8e1fa8bc1df6d04b7dcbb4637f987e263837cd1a
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 16:39:13 +1100

    Update duplicate link spec

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  let(:long_url) { "cultureamp.com" }
-  subject(:cultureamp) { Link.create!(long_url: long_url) }
+  let(:valid_url) { "cultureamp.com" }
+
+  before do
+    @cultureamp = Link.create!(long_url: valid_url)
+  end
 
   it "generates a short URL using id" do
-    expect(cultureamp.short_url).to be_present
+    expect(@cultureamp.short_url).to be_present
   end
 
   it "does not create a new link object" do
-    Link.create!(long_url: long_url)
-    link = Link.create(long_url: long_url)
-    expect(link).to_not be_valid
-    expect(link.errors[:long_url]).to eq(["has already been taken"])
+    dup_link = Link.create(long_url: valid_url)
+    expect(dup_link).to_not be_valid
+    expect(dup_link.errors[:long_url]).to eq(["has already been taken"])
+  end
+
+  it "does not accept invalid URLs" do
+    skip
+    invalid_link = Link.create(long_url: "foobar")
   end
 end
