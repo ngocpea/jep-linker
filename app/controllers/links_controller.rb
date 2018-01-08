c0d0e416e4d80ff5aadc90de7043d8d6a4b6ef12
Jaime Terreu <jaime.terreu@cultureamp.com>
2018-01-08 12:03:54 +1100

    added error feature test

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,9 +8,14 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Services::Url.find_or_create(long_url: params[:long_url])
-    @shortened_link = "Your short link is #{@link.short_url}"
-    redirect_to link_path(@link) 
-    flash[:notice] = "Short link successfully created"
+    empty = Services::Url.is_empty?(long_url: params[:long_url])
+    if empty
+      render plain: "Please enter your link"
+    else
+      @link = Services::Url.find_or_create(long_url: params[:long_url])
+      @shortened_link = "Your short link is #{@link.short_url}"
+      redirect_to link_path(@link) 
+      flash[:notice] = "Short link successfully created"
+    end
   end
 end
--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -8,4 +8,10 @@ RSpec.feature "Creating a new link" do
     link = Link.first
     expect(page).to have_content("Short link successfully created #{link.short_url}")
   end
+
+  scenario "when a user enters no URL they should see an error message" do
+    visit "/"
+    click_button "Shorten"
+    expect(page).to have_content("Please enter your link")
+  end
 end
