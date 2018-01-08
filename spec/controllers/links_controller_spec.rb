4f4b19bb86054b371e2a80ccc4037cedf721b509
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 14:07:02 +1100

    Modify create test and add show test

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
   it "can create a link successfully" do
-    post :create, params: { long_url: "google.com" }
-    expect(response).to be_success
+    post :create, params: { link: { long_url: "google.com" } }
     link = Link.find_by(long_url: "google.com")
     expect(link).to be_present
-    expect(response.body).to eq("Your link has been shortened. Your short link is #{link.short_url}")
+    expect(response).to redirect_to(link_path(link))
+  end
+
+  it "shows a link" do
+    post :create, params: { link: { long_url: "google.com" } }
+    link = Link.find_by(long_url: "google.com")
+    get :show, params: { id: link.id }
+    expect(response).to be_success
   end
 end
