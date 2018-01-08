80ee38db9acbcc813e80e90c6c8359776f63bdd0
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 14:05:00 +1100

    Add show action and link error messages

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,14 +1,24 @@
 class LinksController < ApplicationController
-  def index() end
+  def new
+    @link = Link.new
+  end
 
   def create
-    link = Link.find_or_create_by(link_params)
-    render plain: "Your link has been shortened. Your short link is #{link.short_url}"
+    @link = Link.find_or_create_by(link_params)
+    if @link.valid?
+      redirect_to @link
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @link = Link.find(params[:id])
   end
 
   private
 
   def link_params
-    params.permit(:long_url)
+    params.require(:link).permit(:long_url)
   end
 end
