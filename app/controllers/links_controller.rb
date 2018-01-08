e1ee885d4e9404e1a1454fa6de9cb835178d46cc
Namibia Torres <namibialt@gmail.com>
2018-01-08 14:41:01 +1100

    Completed part 1 of week 2 linker

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,15 +1,17 @@
 class LinksController < ApplicationController
   def index
+    @link = Link.new
   end
 
   def create
-    existing_link = Link.where(long_url: params[:long_url]).first
+    long_url = params[:link][:long_url]
+    existing_link = Link.where(long_url: long_url).first
     if existing_link
       render plain: "Your short link is #{existing_link.short_url}"
     else
-      link = Link.new(long_url: params[:long_url])
+      link = Link.new(long_url: long_url)
       link.valid?
-      return render plain: "please enter your link" if link.errors[:long_url].any?
+      return flash[:alert] = "please enter your link" if link.errors[:long_url].any?
       link.create_short_url
       until link.save
         link.create_short_url
