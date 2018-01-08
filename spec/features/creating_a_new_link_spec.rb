62e731efac3b42ad06a5b35fc984ed4982389449
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 14:07:25 +1100

    Add test for rendering error message

--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -6,6 +6,12 @@ RSpec.feature "Creating a new link" do
     fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
     click_button "Shorten"
     link = Link.last
-    expect(page).to have_content("Your short link is #{link.short_url}")
+    expect(page).to have_content("Your short url is http://short.ly/#{link.short_url}")
+  end
+
+  scenario "shows error message if no link submitted" do
+    visit "/"
+    click_button "Shorten"
+    expect(page).to have_content("Long url can't be blank")
   end
 end
