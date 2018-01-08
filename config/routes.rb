fb6d15aa6cf3808598f4fedfca64f247353e7255
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-08 14:05:42 +1100

    Delete index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'links#index'
+  root 'links#new'
 
-  post "/", to: "links#create"
+  resources :links
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
