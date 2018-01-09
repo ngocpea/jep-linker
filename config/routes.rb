ed0a5e958d0cd689bab1d793476e62a9b37ba503
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-09 15:29:33 +1100

    Remove unnecessary comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root 'links#new'
   resources :links
   get '/:short_url', to: 'links#forward'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
