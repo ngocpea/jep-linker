42d497de89e663cb1ce6dcd161fe1fa7840322eb
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-09 15:27:47 +1100

    Add validation tests for link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,20 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  let(:valid_url) { "cultureamp.com" }
+  let(:long_url) { "cultureamp.com" }
+  let(:custom_url) { "ca" }
 
   before do
-    @cultureamp = Link.create!(long_url: valid_url)
+    @cultureamp = Link.create!(long_url: long_url)
   end
 
-  it "generates a short URL using id" do
+  it "generates a short URL if no custom URL" do
     expect(@cultureamp.short_url).to be_present
   end
 
-  it "does not create a new link object" do
-    dup_link = Link.create(long_url: valid_url)
+  it "does not allow custom URLs that are longer than 20 chars" do
+    too_long = "a" * 21
+    invalid_link = Link.create(long_url: "foo.com", short_url: too_long)
+    expect(invalid_link).to_not be_valid
+  end
+
+  it "downcases long_url and short_url" do
+    mixed_case = Link.create(long_url: "gOOglE.CoM", short_url: "GoOgle")
+    expect(mixed_case.long_url).to eq("google.com")
+    expect(mixed_case.short_url).to eq("google")
+  end
+
+  it "does not create entries with same short_url" do
+    Link.create!(long_url: long_url, short_url: custom_url)
+    dup_link = Link.create(long_url: "test.com", short_url: custom_url)
     expect(dup_link).to_not be_valid
-    expect(dup_link.errors[:long_url]).to eq(["has already been taken"])
+    expect(dup_link.errors[:short_url]).to eq(["has already been taken"])
   end
 
   it "does not accept invalid URLs" do
