ac396c74ff6c1699778f9a6e59ccd6baa73f4e9b
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-09 15:28:36 +1100

    Add custom find_or_create method and short_url param

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,7 +4,7 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.find_or_create_by(link_params)
+    @link = find_or_create(link_params)
     if @link.valid?
       redirect_to @link
     else
@@ -17,14 +17,18 @@ class LinksController < ApplicationController
   end
 
   def forward
-    id = params[:short_url].to_i(36)
-    @link = Link.find(id)
+    @link = Link.find_by(short_url: params[:short_url])
     redirect_to "http://#{@link.long_url}"
   end
 
   private
 
   def link_params
-    params.require(:link).permit(:long_url)
+    params.require(:link).permit(:long_url, :short_url)
+  end
+
+  def find_or_create(link_params)
+    link = Link.find_by(long_url: link_params[:long_url], is_custom_url: false)
+    link || Link.create(link_params)
   end
 end
