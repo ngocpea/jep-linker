a943f7d79fd9ef2ce68e30ed09abc81ca04e8daa
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-09 16:59:59 +1100

    Add URL validation and improve short_code generation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,18 +1,31 @@
 class Link < ApplicationRecord
   before_save :build_short_url, on: :create
   before_save { long_url.downcase! }
+  before_save { short_url.downcase! }
 
-  validates :long_url, presence: true
+  validates :long_url,
+            format: { with: /\A#{URI::regexp(['http', 'https'])}\z/,
+                      message: "Invalid URL format" },
+            presence: true
   validates :short_url,
             uniqueness: true,
             length: { maximum: 20, minimum: 1 },
             allow_blank: true
 
+  private
+
+  def generate_short_code
+    SecureRandom.urlsafe_base64(5).downcase!
+  end
+
   def build_short_url
     if short_url.blank?
-      self.short_url = SecureRandom.urlsafe_base64(5).downcase!
+      short_code = generate_short_code
+      while Link.where(short_url: short_code).exists?
+        short_code = generate_short_code
+      end
+      self.short_url = short_code
     else
-      short_url.downcase!
       self.is_custom_url = true
     end
   end
