951545e6a62df6239b27e4a920d44fe8665c7ca0
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-09 15:29:01 +1100

    Add build_short_url method and validation for short_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,19 @@
 class Link < ApplicationRecord
-  validates :long_url, uniqueness: true, presence: true
-  validates :custom_url, uniqueness: true
-  def short_url
-    id.to_s(36)
+  before_save :build_short_url, on: :create
+  before_save { long_url.downcase! }
+
+  validates :long_url, presence: true
+  validates :short_url,
+            uniqueness: true,
+            length: { maximum: 20, minimum: 1 },
+            allow_blank: true
+
+  def build_short_url
+    if short_url.blank?
+      self.short_url = SecureRandom.urlsafe_base64(5).downcase!
+    else
+      short_url.downcase!
+      self.is_custom_url = true
+    end
   end
 end
