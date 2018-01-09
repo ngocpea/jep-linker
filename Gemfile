9eddb424231ad9397e64dc23a3d35144d9d16df3
Rebecca Vo <beckvo@gmail.com>
2018-01-09 15:45:51 +1100

    tests passing

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -42,6 +43,8 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'rspec-rails'
   gem 'capybara-selenium'
+  gem 'pry'
+  gem 'launchy'
 end
 
 group :development do
--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -2,30 +2,26 @@ require 'rails_helper'
 require 'capybara/dsl'
 
 RSpec.feature "Creating a new link" do
-  before(:context) do
+  before do
     visit "/"
     fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
     click_button "Shorten"
-    #link = Link.first
   end
-    scenario "successfully creates a new link" do
-    # visit "/"
-    # fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
-    # click_button "Shorten"
+
+  scenario "successfully creates a new link" do
     link = Link.first
-    expect(page).to have_content("Short link successfully created #{link.short_url}")
+    find_link("http://www.linker/#{link.short_url}").visible?
   end
 
   scenario "when a user enters no URL they should see an error message" do
     visit "/"
     click_button "Shorten"
-    expect(page).to have_content("Please enter your link")
+    assert_selector "#notice", text: "URL is invalid" 
   end
 
-  scenario "when user clicks short link, the should be redirected back to long link", js: true do
+  scenario "when user clicks short link, they should be redirected back to long link", js: true do
     link = Link.first
-    visit "/links/#{link.id}"
-    click_link "#{link.short_url}"
+    click_link "http://www.linker/#{link.short_url}"
     expect(page).to have_current_path(link.long_url)
   end
 end
