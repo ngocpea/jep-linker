04fffe07446fa42dd43ec9031b6fc5400336f8b0
Rebecca Vo <beckvo@gmail.com>
2018-01-09 11:53:05 +1100

    added Capybara-selenium, updated no links in URL and green tests for redirect

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,7 @@ class Link < ApplicationRecord
   validates :long_url, presence: true
   validates_uniqueness_of  :long_url
   
-  def shorten_url
+  def to_param
+    short_url
   end
 end
--- spec/features/creating_a_new_link_spec.rb
+++ spec/features/creating_a_new_link_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
+require 'capybara/dsl'
 
 RSpec.feature "Creating a new link" do
-  scenario "successfully creates a new link" do
+  before(:context) do
     visit "/"
     fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
     click_button "Shorten"
+    #link = Link.first
+  end
+    scenario "successfully creates a new link" do
+    # visit "/"
+    # fill_in "Long URL", with: "http://ryanbigg.com/2016/04/hiring-juniors"
+    # click_button "Shorten"
     link = Link.first
     expect(page).to have_content("Short link successfully created #{link.short_url}")
   end
@@ -13,5 +20,12 @@ RSpec.feature "Creating a new link" do
     visit "/"
     click_button "Shorten"
     expect(page).to have_content("Please enter your link")
+  end
+
+  scenario "when user clicks short link, the should be redirected back to long link", js: true do
+    link = Link.first
+    visit "/links/#{link.id}"
+    click_link "#{link.short_url}"
+    expect(page).to have_current_path(link.long_url)
   end
 end
