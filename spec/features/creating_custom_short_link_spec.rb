15c23c260cde509e89bec14c8b7c19fa6c06d936
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-15 15:26:27 +1100

    Add correct error messages to tests

--- spec/features/creating_custom_short_link_spec.rb
+++ spec/features/creating_custom_short_link_spec.rb
@@ -13,21 +13,19 @@ RSpec.feature "creating a custom short link" do
   end
 
   it "shows error if short link already in use" do
-    skip
     Link.create!(long_url: long_url, short_url: custom_url)
     visit "/"
     fill_in "Long URL", with: long_url
     fill_in "Custom URL", with: custom_url
     click_button "Shorten"
-    expect(page).to have_content("Custom URL is taken, choose another")
+    expect(page).to have_content("Short url has already been taken")
   end
 
   it "shows error if short link is 'links'" do
-    skip
     visit "/"
     fill_in "Long URL", with: long_url
     fill_in "Custom URL", with: "links"
     click_button "Shorten"
-    expect(page).to have_content("Custom URL is taken, choose another")
+    expect(page).to have_content("Short url is invalid")
   end
 end
