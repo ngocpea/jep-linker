ede5079082a5aac75dd7464e32cf5799a00e41c5
Jasmine Ellis <jasmine.ellis@cultureamp.com>
2018-01-15 15:25:56 +1100

    Fix controller tests

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
-  let(:long_url) { "google.com" }
+  let(:long_url) { "http://google.com" }
   it "can create a link successfully" do
     post :create, params: { link: { long_url: long_url } }
     link = Link.find_by(long_url: long_url)
@@ -19,6 +19,13 @@ RSpec.describe LinksController, type: :controller do
   it "forwards a short url to long url" do
     link = Link.create(long_url: long_url)
     get :forward, params: { short_url: link.short_url }
-    expect(response).to redirect_to("http://#{long_url}")
+    expect(response).to redirect_to(long_url)
+  end
+
+  it "formats long urls" do
+    post :create, params: { link: { long_url: "www.google.com" } }
+    link = Link.find_by(long_url: long_url)
+    expect(link).to be_present
+    expect(link.long_url).to eq("http://google.com")
   end
 end
